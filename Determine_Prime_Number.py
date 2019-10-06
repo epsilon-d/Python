b8839bd81a28072e35edb3f6b2f2ebc8c8575918
epsilon-d <49400045+epsilon-d@users.noreply.github.com>
2019-10-06 19:05:20 +0900

    Update Determine_Prime_Number.py

--- Determine_Prime_Number.py
+++ Determine_Prime_Number.py
@@ -1,9 +1,10 @@
 # This program is a program that determines if a given number is prime.
 
-num = int(input("Please input any integer number: "))
+num: int = int(input("Please input any integer number: "))
 
 for x in range(2, num, 1):
     if num % x == 0:
         print("The number is not prime number.")
+        print("Because the number can be divided by %d." % x)
         exit()
 print("The number is prime number.")
