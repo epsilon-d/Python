0c12911ebd03a1405f0dd0a49194509844b565c5
epsilon-d <49400045+epsilon-d@users.noreply.github.com>
2019-10-11 13:19:49 +0900

    Update Sum_of_Digits.py

--- Sum_of_Digits.py
+++ Sum_of_Digits.py
@@ -1,19 +1,13 @@
 # Practice #7: Sum of Digit
 # This program calculates the sum of the number of digits of the integer.
 
-number = int(input("Please input any integer number: "))
-number_copy = number
-solution = 0
-i = 0
+num = int(input("Please input any integer number: "))
 
-while number_copy != 0:
-    number_copy = int(number_copy // 10)
-    i = i + 1
+sum_num = 0
 
-while i >= 1:
-    digit = number // (10 ** (i - 1))
-    solution = solution + digit
-    number = number - (digit * (10 ** (i - 1)))
-    i = i - 1
+while num > 0:
+    x = num % 10
+    sum_num = sum_num + x
+    num = num // 10
 
-print("Sum of digit is %d." % solution)
+print("Sum of digit is %d." % sum_num)
