ffd9c503ff10942c5bba12c8361b1a7f6b2fe33a
epsilon-d <eumyangsun3@naver.com>
2021-06-24 23:53:56 +0900

    commit

--- Algorithm/Programmers/Level_01/mock_exam.py
+++ Algorithm/Programmers/Level_01/mock_exam.py
@@ -1,6 +1,6 @@
 # Copyright 2021 epsilon-d. All rights reserved.
 # https://programmers.co.kr/learn/courses/30/lessons/42840
-# 모의고사
+# 프로그래머스 Level 1. 모의고사
 
 def solution(answers):
     people = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
