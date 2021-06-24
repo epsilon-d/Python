3a90cf6a2d9fc8b5fd0e5235863d5fb842f896e9
epsilon-d <eumyangsun3@naver.com>
2021-06-25 00:04:31 +0900

    commit

---
+++ Algorithm/Programmers/Level_01/inner_product.py
@@ -0,0 +1,9 @@
+# Copyright 2021 epsilon-d. All rights reserved.
+# https://programmers.co.kr/learn/courses/30/lessons/70128
+# 프로그래머스 Level 1. 내적
+
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+    return answer
