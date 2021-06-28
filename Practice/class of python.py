5ac34eb95f04c84fcfc9be77878c2b6baf78d055
epsilon-d <eumyangsun3@naver.com>
2021-06-29 00:42:08 +0900

    commit

--- Practice/class of python.py
+++ Practice/class of python.py
@@ -10,6 +10,7 @@ class Calculator:
         self.result -= num
         return self.result
 
+
 cal1 = Calculator()
 cal2 = Calculator()
 cal3 = Calculator()
@@ -23,15 +24,18 @@ result1 = 0
 result2 = 0
 result3 = 0
 
+
 def add1(num):
     global result1
     result1 += num
     return result1
 
+
 def add2(num):
     global result2
     result2 += num
     return result2
+
 
 def add3(num):
     global result3
