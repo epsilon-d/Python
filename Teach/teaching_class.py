97300e742a79ad9a6dc1765f58e39de3a0027be7
epsilon-d <eumyangsun3@naver.com>
2021-07-04 23:39:29 +0900

    commit

--- Teach/teaching_class.py
+++ Teach/teaching_class.py
@@ -1,5 +1,24 @@
 
 class Animal:
+    def __init__(self):
+        self.age = 0
+        self.leg = 4
+        self.wing = "no"
+
+    def shout(self):
+        print("growl")
 
 
+class Duck(Animal):
+    def __init__(self):
+        super().__init__()
+        self.leg = 2
+        self.wing = "yes"
 
+    def shout(self):
+        print("꽥꽥")
+
+
+a = Duck()
+a.age = 5
+print(a.age)
