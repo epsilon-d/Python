5dc77631a6dc0069caa2e9792c4bd20c327859f2
epsilon-d <eumyangsun3@naver.com>
2021-07-06 22:17:33 +0900

    commit

--- Algorithm/Baekjoon/python_input.py
+++ Algorithm/Baekjoon/python_input.py
@@ -1,5 +1,6 @@
 # https://www.acmicpc.net/problem/15552
 # https://www.acmicpc.net/problem/11021
+# https://www.acmicpc.net/problem/11022
 # 빠르게 입력받기
 
 import sys
@@ -8,4 +9,4 @@ T = int(input())
 
 for i in range(T):
     A, B = map(int, sys.stdin.readline().split())
-    print(f"Case #{i + 1}: {A + B}")
+    print(f"Case #{i + 1}: {A} + {B} = {A + B}")
