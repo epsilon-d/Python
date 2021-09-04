4b81e54ef5e464c0752d0be7313d5f207ef071ce
epsilon-d <eumyangsun3@naver.com>
2021-09-04 21:25:32 +0900

    commit

--- Algorithm/Baekjoon/Problem_4948.py
+++ Algorithm/Baekjoon/Problem_4948.py
@@ -1,20 +1,28 @@
 # https://www.acmicpc.net/problem/4948
 
+prime_list = []
+for i in range(2, 246913):
+    stop = 0
+    if i < 100:
+        k = i
+    else:
+        k = int(i ** (1/2)) + 1
+    for j in range(2, k):
+        if i % j == 0:
+            stop = 1
+            break
+    if stop == 0:
+        prime_list.append(i)
+
 while True:
     n = int(input())
+    cnt = 0
     if n == 0:
         break
-    prime_number = 0
-    for i in range(n + 1, (2 * n) + 1):
-        stop = 0
-        if i < 100:
-            k = i
-        else:
-            k = int(i ** (1/2)) + 1
-        for j in range(2, k):
-            if i % j == 0:
-                stop = 1
-                break
-        if stop == 0:
-            prime_number += 1
-    print(prime_number)
+    for m in range(len(prime_list)):
+        if prime_list[m] <= n:
+            continue
+        if prime_list[m] > n * 2:
+            break
+        cnt = cnt + 1
+    print(cnt)
